5f961ef70c1202b91c8999346bbb81fae492af19
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-05-01 19:52:06 +0700

    update LeafNode constructor to accept tag as the first argument

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,7 +21,7 @@ class HTMLNode:
     
 
 class LeafNode(HTMLNode):
-    def __init__(self, value, tag=None, props=None):
+    def __init__(self, tag, value, props=None):
         super().__init__(tag, value, None, props)
         
     def to_html(self):
@@ -30,4 +30,7 @@ class LeafNode(HTMLNode):
         if self.tag is None:
             return f"{self.value}"
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
     
