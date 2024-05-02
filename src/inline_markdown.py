10428354f5aa7d5f34540042524438a1ae0f8e5b
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-05-02 21:17:17 +0700

    reorganize inline_markdown.py functions

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -11,6 +11,16 @@ from textnode import (
 )
 
 
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
+    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
+    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
+
 def split_nodes_delimiter(old_nodes: list[TextNode], delimiter, text_type):
     new_nodes = []
     for old_node in old_nodes:
@@ -30,14 +40,6 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter, text_type):
             else:
                 new_nodes.append(TextNode(splitted_text, text_type_text))
     return new_nodes
-
-
-def extract_markdown_images(text):
-    return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
-
-
-def extract_markdown_links(text):
-    return re.findall(r"\[(.*?)\]\((.*?)\)", text)
 
 
 def split_nodes_image(old_nodes: list[TextNode]):
@@ -99,11 +101,9 @@ def split_nodes_link(old_nodes: list[TextNode]):
     return new_nodes
 
 
-def text_to_textnodes(text):
-    nodes = [TextNode(text, text_type_text)]
-    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
-    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
-    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
-    nodes = split_nodes_image(nodes)
-    nodes = split_nodes_link(nodes)
-    return nodes
+def extract_markdown_images(text):
+    return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+
+
+def extract_markdown_links(text):
+    return re.findall(r"\[(.*?)\]\((.*?)\)", text)
