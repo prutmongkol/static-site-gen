62a9019db0a96ce2494246ebf1258780cbd4f6bc
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-05-03 16:54:18 +0700

    add generate_page function to generate html file from markdown file

--- src/copy_contents.py
+++ src/copy_contents.py
@@ -2,7 +2,10 @@ import os
 import shutil
 
 
-def copy_contents(src, dst):
+from markdown_blocks import markdown_to_html_node
+
+
+def copy_files(src, dst):
     ls = os.listdir(src)
     
     if not os.path.exists(dst):
@@ -15,4 +18,35 @@ def copy_contents(src, dst):
             print(f'Copying: {src_path} -> {dst_path}')
             shutil.copy(src_path, dst_path)
         else:
-            copy_contents(src_path, dst_path)
+            copy_files(src_path, dst_path)
+            
+
+def extract_title(markdown):
+    first_line = markdown.readline()
+    if first_line[0:2] != "# ":
+        raise ValueError("First line must be h1 header (# )")
+    return first_line[2:]
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    
+    template = None
+    with open(template_path) as f:
+        template = f.read()
+    
+    markdown = open(from_path)
+    title = extract_title(markdown)
+    content = markdown.read()
+    markdown.close()
+    
+    content_html = markdown_to_html_node(content).to_html()
+    
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", content_html)
+    
+    if not os.path.exists(os.path.dirname(dest_path)):
+        os.makedirs(os.path.dirname(dest_path))
+    
+    with open(dest_path, 'w') as f:
+        f.write(template)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import os
 import shutil
 
-from copy_contents import copy_contents
+from copy_contents import copy_files, generate_page
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+file_path_content = "./content/index.md"
+file_path_template = "./template.html"
+file_path_public = "./public/index.html"
 
 
 def main():
@@ -13,7 +16,9 @@ def main():
         shutil.rmtree(dir_path_public)
         
     print("Copying static files to public directory...")
-    copy_contents(dir_path_static, dir_path_public)
+    copy_files(dir_path_static, dir_path_public)
 
+    generate_page(file_path_content, file_path_template, file_path_public)
+    
 
 main()
